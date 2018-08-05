ce433b4cf3946f0663a01fe8a3f50078fbc57194
Shannon Smith <shannon@powerboard.co.nz>
2018-08-05 07:27:24 -0500

    First Commit

---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-transformer-remark`,
+    'gatsby-plugin-eslint',
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: `2f10bdfdabc47fc2f76f98ef230ceb`,
+        previewMode: true,
+        disableLiveReload: false,
+      },
+    }
+
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+}
