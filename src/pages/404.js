d2f36f6a6ec39d3fefda201836d6d90245b654bd
Shannon Smith <shannon@powerboard.co.nz>
2018-08-29 16:30:02 -0500

    Customise 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { Section, Container } from '../components/common'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Section lightBackground>
+      <Container maxWidth="900px">
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Container>
+    </Section>
   </Layout>
 )
 
