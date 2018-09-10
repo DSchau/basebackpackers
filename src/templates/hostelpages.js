3d769229e09acb8754dc2b0a5719df77ef364b76
Shannon Smith <shannon@powerboard.co.nz>
2018-09-10 17:34:52 -0500

    Add in dyamic hostels mews

--- src/templates/hostelpages.js
+++ src/templates/hostelpages.js
@@ -1,11 +1,13 @@
 
 import React from 'react'
+import {Helmet} from "react-helmet";  
 import { HelmetDatoCms } from 'gatsby-source-datocms'
 import { graphql } from 'gatsby' 
 import Img from 'gatsby-image'
 
 import Layout from '../components/layout'
 import { Section, Container } from '../components/common';
+
 
 
 export default class HostelPage extends React.Component {
@@ -17,10 +19,18 @@ export default class HostelPage extends React.Component {
     <Layout>
       <Section lightBackground>
           <HelmetDatoCms seo={data.datoCmsHostel.seoMetaTags} /> 
+          <Helmet>
+
+            <script>{`(function(m,e,w,s){c=m.createElement(e);c.onload=function(){
+Mews.D.apply(null,s)};c.async=1;c.src=w;t=m.getElementsByTagName(e)[0];t.parentNode.insertBefore(c,t);})
+(document,'script','https://www.mews.li/distributor/distributor.min.js',[['${data.datoCmsHostel.mewsId}']]);`}</script>
+
+          </Helmet>
           <Container maxWidth="900px">
             <Img fluid={data.datoCmsHostel.featuredImage.fluid} />
             <h1>{data.datoCmsHostel.title}</h1>
             <div dangerouslySetInnerHTML={{ __html: data.datoCmsHostel.intro }} />
+            <button className="distributor">Book now</button>
           </Container>
           <Container maxWidth="900px" col="4" gap="10px">
               {data.datoCmsHostel.hostelGallery.map(( photo, index) => {
