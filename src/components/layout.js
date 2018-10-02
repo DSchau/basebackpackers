b4885ecd1ddbd0183748e69542a783d43d232a63
Shannon Smith <shannon@powerboard.co.nz>
2018-10-02 11:49:44 -0500

    Fix safari 10 issue

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,41 +1,38 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
-import styled, {ThemeProvider} from 'styled-components'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { StaticQuery, graphql } from 'gatsby';
+import styled, { ThemeProvider } from 'styled-components';
 
-import { Footer } from '../components/layout/index.js'
+import { Footer } from '../components/layout/index.js';
 
-import baseTheme from './theme/base'
+import baseTheme from './theme/base';
 
-import './layout.css'
+import './layout.css';
 
 const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 25%);
   grid-template-rows: repeat(10, auto);
   grid-template-areas:
-    
-    "body body body body"
-    "foot foot foot foot" ;
+    'body body body body'
+    'foot foot foot foot';
 
-    @media (max-width: 768px) {
+  @media (max-width: 768px) {
     grid-template-columns: repeat(2, 50%);
     grid-template-areas:
-    
-    "body body"
-    "foot foot"
-    "foot foot" ;
-
+      'body body'
+      'foot foot'
+      'foot foot';
   }
-    
 `;
 
 const Body = styled.main`
- grid-area: body;
- background:#FCFCFC;
+  grid-area: body;
+  background: #fcfcfc;
+  position: relative;
+  width: 100%;
 `;
-
 
 const Layout = ({ children, data }) => (
   <StaticQuery
@@ -54,26 +51,24 @@ const Layout = ({ children, data }) => (
           title={data.site.siteMetadata.title}
           meta={[
             { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { name: 'keywords', content: 'sample, something' }
           ]}
         >
           <html lang="en" />
         </Helmet>
         <ThemeProvider theme={baseTheme}>
-        <Container>
-            <Body>
-              {children}
-            </Body>
-          <Footer />
-        </Container>
+          <>
+            <Body>{children}</Body>
+            <Footer />
+          </>
         </ThemeProvider>
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
