053146f66cfe91819c31b579a6c8635d67116c13
Shannon Smith <shannon@powerboard.co.nz>
2018-10-02 12:42:26 -0500

    Add in GTM

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,6 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
     siteUrl: 'https://basebackpackers.com'
-
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -11,6 +10,20 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-remark`,
     {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: '	GTM-T8V6H33',
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: true
+
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
+      }
+    },
+    {
       resolve: `gatsby-plugin-sitemap`
     },
     {
@@ -18,7 +31,7 @@ module.exports = {
       options: {
         apiToken: `2f10bdfdabc47fc2f76f98ef230ceb`,
         previewMode: false,
-        disableLiveReload: false,
+        disableLiveReload: false
       }
     },
 
@@ -31,10 +44,10 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
+        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+      }
     },
     `gatsby-plugin-twitter`,
     `gatsby-plugin-netlify`
-  ],
-}
+  ]
+};
